7946d8cf79ed2f019e28c7c5594d4bcf2e6c2086
salvable <salvable@kakao.com>
2021-08-09 18:34:43 +0900

    입장, 퇴장 메세지 삭제. chat 기능 완료

--- app.js
+++ app.js
@@ -16,19 +16,16 @@ const io = socketIo(server,{
 io.on("connection", (socket) => {
     // connection 요청시 roomName과 userName을 보내줌
     socket.on("join", ({roomName: room, userName: user}) => {
-        console.log(room,user)
         // 받아온 roomName으로 입장
         socket.join(room);
-        // 해당 room에 전송
-        io.to(room).emit("onConnect", `${user} 님이 입장했습니다.`);
         // 해당 룸에 메세지 전송
         socket.on("onSend", (messageItem) => {
+            console.log(messageItem)
             io.to(room).emit("onReceive", messageItem);
         });
 
         socket.on("disconnect", () => {
             socket.leave(room);
-            io.to(room).emit("onDisconnect", `${user} 님이 퇴장하셨습니다.`);
         });
     });
 })
