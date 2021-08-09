18cd7a294951fa88f4c196a2502e3881dfc57ef1
salvable <salvable@kakao.com>
2021-08-09 17:44:01 +0900

    cors 설정

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
-const port = 6000;
+const port = 3002;
 const app = express();
+const cors = require('cors')
 
 const server = http.createServer(app);
 const io = socketIo(server);
+app.use(cors());
 
 io.on("connection", (socket) => {
+    console.log(socket)
     // connection 요청시 roomName과 userName을 보내줌
     socket.on("join", ({roomName: room, userName: user}) => {
+        console.log(room,user)
         // 받아온 roomName으로 입장
         socket.join(room);
-
         // 해당 room에 전송
         io.to(room).emit("onConnect", `${user} 님이 입장했습니다.`);
-
         // 해당 룸에 메세지 전송
         socket.on("onSend", (messageItem) => {
             io.to(room).emit("onReceive", messageItem);
